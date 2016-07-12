492784083bd70eb1da7c397e1e98f722dab2a202
spock1104 <spock1104@gmail.com>
2016-07-11 21:56:22 -0400

    whoshotjr2006/touchpro's radio search fix

--- rootdir/init.qcom.ril.sh
+++ rootdir/init.qcom.ril.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
 export PATH=/system/xbin:$PATH
 
-setprop gsm.version.baseband `strings -n 25 /firmware/image/modem.b21 | grep -E -o 'MPSS.DI.*\w' | head -1| cut -d "_" -f-1`
+setprop gsm.version.baseband `strings -n 25 /firmware/image/modem.b21 | grep -E -o 'YQC_IMAGE_VERSION_STRING=MPSS.DI.*' | tail -c +26`
